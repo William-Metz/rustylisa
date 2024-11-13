7008d4071572c6a1dd7aecd290f74b3caa463dd1
Will Metz <44082458+William-Metz@users.noreply.github.com>
2024-11-13 14:50:10 -0800

    removed some git hooks

--- src/vcalculator/vcalculator.rs
+++ src/vcalculator/vcalculator.rs
@@ -1,8 +1,3 @@
-// Project: Gravitational Wave Visualizer
-// Author: Will Metz(Pomona College)
-// Created: 2024
-// File Path: src/vcalculator/vcalculator.rs
-// ------------------------------------------------------------
 // This project is open for educational and personal use only.
 // No warranty is provided, and this is not an official product.
 // ------------------------------------------------------------
